aad170a964b224e152d25b12a9c6ae341f1fa8b0
heihuihui1 <727197641@qq.com>
2023-01-18 16:38:56 -0800

    Initial test
    Initial test

--- src/main/java/com/skillup/SkillUpApplication.java
+++ src/main/java/com/skillup/SkillUpApplication.java
@@ -8,5 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SkillUpApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkillUpApplication.class, args);
+        System.out.println("Hi this is Yinfei Zhang's repo");
     }
 }
