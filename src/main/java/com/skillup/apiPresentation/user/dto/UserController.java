25481d01e8b8a0f9ccfb0453d264f666cc563da5
Runze Li <a397704290@outlook.com>
2023-01-22 15:57:30 -0500

    API setup

--- src/main/java/com/skillup/apiPresentation/user/dto/UserController.java
+++ src/main/java/com/skillup/apiPresentation/user/dto/UserController.java
@@ -1,11 +1,13 @@
 package com.skillup.apiPresentation.user.dto;
 
+import com.skillup.apiPresentation.user.util.SkillResponseUtil;
 import com.skillup.apiPresentation.user.util.SkillUpResponse;
 import com.skillup.domain.UserDomain;
 import com.skillup.domain.UserDomainService;
 import com.skillup.apiPresentation.user.dto.in.UserInDto;
 import com.skillup.apiPresentation.user.dto.out.UserOutDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,15 +23,18 @@ public class UserController {
     UserDomainService userDomainService;
 
     @PostMapping
-    public SkillUpResponse createUser(@RequestBody UserInDto userInDto){
+    public ResponseEntity<SkillUpResponse> createUser(@RequestBody UserInDto userInDto){
         //crete userDomain
-        UserDomain userDomain = toDomain(userInDto);
+        UserDomain savedUserDomain = null;
        //call domain service to create user
-        UserDomain savedUserDomain = userDomainService.registry(userDomain);
+        try{
+            savedUserDomain = userDomainService.registry(toDomain(userInDto));
+        }catch (Exception e){
+            return ResponseEntity.status(SkillResponseUtil.BAD_REQUEST).body(SkillUpResponse.builder().msg(SkillResponseUtil.USER_EXISTS).build());
+        }
+
         // create outDto
-        return SkillUpResponse.builder()
-                .result(toOutDto(savedUserDomain))
-                .build();
+         return ResponseEntity.status(SkillResponseUtil.SUCCESS).body(SkillUpResponse.builder().result(toOutDto(savedUserDomain)).build());
     }
 
     private UserDomain toDomain(UserInDto userInDto) {
