c4a1fd35fa4ef36288b6369e85a067296864bbfc
Runze Li <a397704290@outlook.com>
2023-02-02 20:55:07 -0500

    PromotionService

--- src/main/java/com/skillup/domain/promotion/PromotionService.java
+++ src/main/java/com/skillup/domain/promotion/PromotionService.java
@@ -3,12 +3,18 @@ package com.skillup.domain.promotion;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 @Service
 public class PromotionService {
+
     @Autowired
     PromotionRepository promotionRepository;
+
+    @Resource(name = "${promotion.stock-strategy}")
+    StockOperation stockOperation;
+
     public PromotionDomain createPromotion(PromotionDomain promotionDomain) {
         promotionRepository.createPromotion(promotionDomain);
         return promotionDomain;
@@ -21,4 +27,17 @@ public class PromotionService {
     public List<PromotionDomain> getPromotionByStatus(Integer status) {
         return promotionRepository.getPromotionByPromotionStatus(status);
     }
+
+    public boolean lockStock(String id) {
+        return stockOperation.lockStock(id);
+    }
+
+    public boolean deductStock(String id) {
+        return stockOperation.deductStock(id);
+    }
+
+    public boolean revertStock(String id) {
+        return stockOperation.revertStock(id);
+    }
 }
+
