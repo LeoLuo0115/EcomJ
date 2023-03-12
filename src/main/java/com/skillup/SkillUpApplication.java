5223999636ac0ad31ab5915094b3f4f8b7d5452f
leoluo <leoluo0115@gmail.com>
2023-03-12 18:10:45 -0500

    config maven

--- src/main/java/com/skillup/SkillUpApplication.java
+++ src/main/java/com/skillup/SkillUpApplication.java
@@ -2,9 +2,12 @@ package com.skillup;
 
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SkillUpApplication {
     public static void main(String[] args) {
         SpringApplication.run(SkillUpApplication.class, args);
